0d1e627bacc205c1c05d873fefb4f165505f5fa2
vmitache <v.mitache@gmail.com>
2017-11-03 10:19:22 +0200

    ThreadLocal example in CustomerService

--- BC22/src/main/java/curs/banking/business/CustomerService.java
+++ BC22/src/main/java/curs/banking/business/CustomerService.java
@@ -18,10 +18,24 @@ import curs.banking.model.Transaction;
 import curs.banking.model.TransactionType;
 
 public class CustomerService {
+  // ASSOCIATES ONE CONNECTION TO INE THREAD
+  
+  private static ThreadLocal<Connection> mThreadConn = new ThreadLocal<Connection>() {
+    protected Connection initialValue() {
+      try {
+        return DriverManager.getConnection(DB_URL, "SA", "");
+      } catch (SQLException e) {
+        e.printStackTrace();
+        return null;
+      }
+    }
+  };
+
   static final String DB_URL = "jdbc:h2:~/test;AUTO_SERVER=TRUE";
 
   public Connection getConnection() throws Exception {
-    return DriverManager.getConnection(DB_URL, "SA", "");
+    // return DriverManager.getConnection(DB_URL, "SA", "");
+    return mThreadConn.get();
   }
   // SE DAU
   // ACCOUNT1 - findById (1)
@@ -31,26 +45,26 @@ public class CustomerService {
   // 2 - ACCOUNT1.SOLD = ACCOUNT1.SOLD - SUMA
   // 3 - Transaction('C',ACCOUNT2, SUMA)
   // 4 - ACCOUNT2.SOLD = ACCOUN2.SOLA + SUMA
-  
-  
-  public boolean transferMoney(long pAccountDebitId,long pAccountCreditId,double pAmount) throws SQLException, Exception {
+
+  public boolean transferMoney(long pAccountDebitId, long pAccountCreditId, double pAmount)
+      throws SQLException, Exception {
     try (Connection conn = getConnection()) {
       conn.setAutoCommit(false);
-      AccountDAO  accDAO = new AccountDAO(conn);
+      AccountDAO accDAO = new AccountDAO(conn);
       TransactionDAO transDAO = new TransactionDAO(conn);
       Account accDebit = accDAO.findById(pAccountDebitId);
       Account accCredit = accDAO.findById(pAccountCreditId);
-      if(!accDebit.getCurrency().equals(accCredit.getCurrency())) {
+      if (!accDebit.getCurrency().equals(accCredit.getCurrency())) {
         throw new DAOException("Different currency type");
       }
-      if(accCredit.getAmount() >= pAmount) {
+      if (accCredit.getAmount() >= pAmount) {
         long transactionTS = new Date().getTime();
         Transaction trans1 = new Transaction();
         trans1.setAccount(accCredit);
         trans1.setAmount(pAmount);
         trans1.setTransactionTime(new Timestamp(transactionTS));
         trans1.setTransactionType(TransactionType.D);
-        
+
         Transaction trans2 = new Transaction();
         trans2.setAccount(accDebit);
         trans2.setAmount(pAmount);
@@ -73,7 +87,6 @@ public class CustomerService {
       }
     }
   }
-  
 
   public Customer createCustomer(Customer pCustomer, boolean pCommit) throws Exception {
     Connection conn = getConnection();
@@ -96,6 +109,39 @@ public class CustomerService {
     } finally {
       SQLUtils.closeQuietly(conn);
     }
+  }
+
+  public static void main(String[] pArgs) throws Exception {
+    CustomerService cs = new CustomerService();
+    System.out.println(cs.getConnection());
+    for (int i = 0; i < 10; i++) {
+      Thread.sleep(100);
+    }
+    System.out.println(cs.getConnection());
+    new Thread(() -> {
+      CustomerService cs1 = new CustomerService();
+      try {
+        System.out.println(cs1.getConnection());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();
+    new Thread(() -> {
+      CustomerService cs1 = new CustomerService();
+      try {
+        System.out.println(cs1.getConnection());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();
+    new Thread(() -> {
+      CustomerService cs1 = new CustomerService();
+      try {
+        System.out.println(cs1.getConnection());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();
   }
 
 }
